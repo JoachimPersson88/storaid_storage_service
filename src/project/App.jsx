32f995ef8042bba228fc7f9f64d2325fdc8eb924
JoachimPersson88 <joachim.persson88@gmail.com>
2025-10-18 21:18:10 +0200

    "Home_Header" 'App.jsx, main.jsx,  Header.jsx, header.css, index.css' Gjorde en del omstruktuering i maparna för att jag insåg det behövdes för framtida filer. La till behövande logos och ikoner till headern.

--- src/project/App.jsx
+++ src/project/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './components/interface/assets/react.svg'
 import viteLogo from '/vite.svg'
-import '../project/components/interface/styles/bas.css'
+import '../project/components/interface/styles/main.css'
 
 function App() {
   const [count, setCount] = useState(0)
