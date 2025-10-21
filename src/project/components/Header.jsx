839e4c24760eecfe9ad52653edc5a4ec4f0a256f
JoachimPersson88 <joachim.persson88@gmail.com>
2025-10-21 21:08:44 +0200

    "Home_Header" 'Header.jsx, header.css' Jag har skapat CSS för bg-delen och gjort liten error fix i Header.jsx.

--- src/project/components/Header.jsx
+++ src/project/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header>
       <div className="bg">
         <div className="bg-area">
-          <div className="bg-left">
+          <div className="bg-left body-text-4">
             <a className="bg-item">
               <img src="/src/project/interface/assets/Icon_Phone.svg" alt=""/>
               +46 8 123 122 44
@@ -37,13 +37,13 @@ export default function Header() {
             <img src="/src/project/interface/assets/Logo.svg" alt="StorAid logo" />
           </a>
 
-          <nav className="navlinks" aria-label="Primary">
+          <nav className="navlinks body-text-3" aria-label="Primary">
             <a className="link is-active" href="/">Home</a>
             <a className="link" href="/about">About Us</a>
             <a className="link" href="/services">Services</a>
             <a className="link" href="/contact">Contact Us</a>
           </nav>
-            <button>Book Now</button>
+            <button className="btn-book body-text-2" type="button">Book Now</button>
         </div>
       </div>
     </header>
