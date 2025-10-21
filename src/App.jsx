5ac40e54366bf594f3081855f27ad2b9458f9ad1
JoachimPersson88 <joachim.persson88@gmail.com>
2025-10-21 21:00:17 +0200

    "Home_Header" 'index.html, App.jsx, main.jsx, Header.jsx, index.css' la till font-family i index.css för headers och brödtext. Skapt html innehåll för Headers.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./project/components/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+}
---
+++ src/project/components/Header.jsx
@@ -0,0 +1,51 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="bg">
+        <div className="bg-area">
+          <div className="bg-left">
+            <a className="bg-item">
+              <img src="/src/project/interface/assets/Icon_Phone.svg" alt=""/>
+              +46 8 123 122 44
+            </a>
+            <a className="bg-item">
+              <img src="/src/project/interface/assets/Icon_Mail.svg" alt=""/>
+              contact@domain.com
+            </a>
+          </div>
+
+          <div className="bg-right">
+            <a className="social" href="#" aria-label="Facebook">
+              <img src="/src/project/interface/ui/Facebook.svg" alt="" />
+            </a>
+            <a className="social" href="#" aria-label="X">
+              <img src="/src/project/interface/ui/Humble.svg" alt="" />
+            </a>
+            <a className="social" href="#" aria-label="Instagram">
+              <img src="/src/project/interface/ui/Instagram.svg" alt="" />
+            </a>
+            <a className="social" href="#" aria-label="YouTube">
+              <img src="/src/project/interface/ui/Youtube.svg" alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="navbar">
+        <div className="nav-area">
+          <a href="/">
+            <img src="/src/project/interface/assets/Logo.svg" alt="StorAid logo" />
+          </a>
+
+          <nav className="navlinks" aria-label="Primary">
+            <a className="link is-active" href="/">Home</a>
+            <a className="link" href="/about">About Us</a>
+            <a className="link" href="/services">Services</a>
+            <a className="link" href="/contact">Contact Us</a>
+          </nav>
+            <button>Book Now</button>
+        </div>
+      </div>
+    </header>
+  );
+}
