7886349d0f0225e31f885e9b402a3eaeaad60c0a
JoachimPersson88 <joachim.persson88@gmail.com>
2025-10-27 18:27:28 +0100

    "Home_Our_Services" 'Our_Services.jsx' Har skapat strukturen för jsx-filen.

---
+++ src/project/components/Our_Services.jsx
@@ -0,0 +1,51 @@
+export default function Hero() {
+    return (
+        <section className="our_services">
+            <div className="container">
+                <h4>Our Services</h4>
+                <div className="our_services_area">
+                    <h3>Specialized Storage for Every Special Item</h3>
+                    <p className="body-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo.
+                        Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.
+                    </p>
+                    <div className="our_services_grid">
+                        <div className="item1">
+                            <h4>Diverse Unit Sizes</h4>
+                            <p className="body-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                            </p>
+                        </div>
+                        <div className="item2">
+                            <h4>Moving Assistance</h4>
+                            <p className="body-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                            </p>
+
+                        </div>
+                        <div className="item3">
+                            <h4>Vehicle Storage</h4>
+                            <p className="body-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                            </p>
+
+                        </div>
+                        <div className="item4">
+                            <h4>Top-Notch Security</h4>
+                            <p className="body-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                            </p>
+
+                        </div>
+                        <img src="/src/project/interface/assets/bg_img.svg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
