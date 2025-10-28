562132c7e0c65e6f44954fa153bc5e527148052f
JoachimPersson88 <joachim.persson88@gmail.com>
2025-10-28 20:08:17 +0100

    "Home_Testimonials START" 'Testimonial.jsx, index.css, testimonials.css, Home.jsx' Har skapat alla filer och fixat pathingen, samt även lagt till bilder och ikoner

--- src/project/pages/Home.jsx
+++ src/project/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/Hero";
 import About_Us from "../components/About_Us";
 import Brands from "../components/Brands";
 import Our_Services from "../components/Our_Services";
+import Testimonials from "../components/Testimonials";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       <About_Us />
       <Brands />
       <Our_Services />
-      <p>TESTIMONIALS</p>
+      <Testimonials />
       <p>WHY CHOOSE US</p>
       <p>PRICING PLAN</p>
       <p>LOOKING FOR SECURE</p>
