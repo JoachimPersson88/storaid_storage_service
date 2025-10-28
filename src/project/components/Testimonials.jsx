f7496dec3c3a759deb951c6db9ab55c331478dda
JoachimPersson88 <joachim.persson88@gmail.com>
2025-10-28 20:09:31 +0100

    "Home_Testimonials" 'Testimonials.jsx' Har skapat strukturen.

--- src/project/components/Testimonials.jsx
+++ src/project/components/Testimonials.jsx
@@ -1,7 +1,40 @@
 export default function Testimonials() {
     return (
         <section className="testimonials">
+            <div className="container">
+                <div className="testimonials_area">
+                    <h4></h4>
+                    <h3></h3>
 
+                    <div className="testimonials_grid">
+                        <div className="item1">
+                            <img src="" alt="" />
+                            <p className="body-text"></p>
+                            <img src="" alt="" />
+                            <h6></h6>
+                            <p className="role"></p>
+                            <img src="" alt="" />
+                        </div>
+                        <div className="item2">
+                            <img src="" alt="" />
+                            <p className="body-text"></p>
+                            <img src="" alt="" />
+                            <h6></h6>
+                            <p className="role"></p>
+                            <img src="" alt="" />
+                        </div>
+                        <div className="item3">
+                            <img src="" alt="" />
+                            <p className="body-text"></p>
+                            <img src="" alt="" />
+                            <h6></h6>
+                            <p className="role"></p>
+                            <img src="" alt="" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </section>
     );
 }
