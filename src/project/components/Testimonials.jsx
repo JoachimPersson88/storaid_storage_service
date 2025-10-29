0707615f1607f889b5c3c517306556738f8c11d9
JoachimPersson88 <joachim.persson88@gmail.com>
2025-10-29 19:37:21 +0100

    "Home_Testimonials" 'Testimonial.jsx, testimonial.css' Efter mycket om och men så har jag fått till stylingen. Jag hade problem med bakgrunden, som jag sedan fick ta hjälp av chatGPT. Fick lite lärdomar om layer-hantering av chatGPT.

--- src/project/components/Testimonials.jsx
+++ src/project/components/Testimonials.jsx
@@ -1,6 +1,8 @@
 export default function Testimonials() {
     return (
         <section className="testimonials">
+            
+            <img className="Bg" src="/src/project/interface/assets/Testimonials_Bg.svg" alt="" />
             <div className="container">
                 <div className="testimonials_area">
 
@@ -9,48 +11,64 @@ export default function Testimonials() {
                     <p className="body-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo.
-                        Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.
+                        Mauris ac blandit
+                        <br/>nisi non sodales augue. Phasellus eget elit gravida.
                         </p>
 
                     <div className="testimonials_grid">
 
-                        <div className="item1">
+                        <div className="item">
                             <img className="stars" src="/src/project/interface/ui/4_stars.svg" alt="" />
                             <p className="body-text">
                                 Lorem ipsum dolor sit amet, consectetur adipis 
                                 cing elit. Ut libero lectus, porta nec turpis sit 
                                 amet, lobortis fringilla ante.
                                 </p>
-                            <img className="avatar" src="/src/project/interface/ui/avatar.svg" alt="" />
-                            <h6>Aiden Harvey</h6>
-                            <p className="role">Customer</p>
-                            <img className="quotes" src="/src/project/interface/ui/quotes.svg" alt="" />
-                        </div>
-
-                        <div className="item2">
-                            <img className="stars" src="/src/project/interface/ui/5_stars.svg" alt="" />
-                            <p className="body-text">
-                                Lorem ipsum dolor sit amet, consectetur adipis 
-                                cing elit. Ut libero lectus, porta nec turpis sit 
-                                amet, lobortis fringilla ante.                                
-                            </p>
-                            <img className="avatar" src="/src/project/interface/ui/avatar.svg" alt="" />
-                            <h6>Carrisa Jocelyn</h6>
-                            <p className="role">Customer</p>
-                            <img className="quotes" src="/src/project/interface/ui/quotes.svg" alt="" />
-                        </div>
-
-                        <div className="item3">
-                            <img className="stars" src="/src/project/interface/ui/5_stars.svg" alt="" />
-                            <p className="body-text">
-                                Lorem ipsum dolor sit amet, consectetur adipis 
-                                cing elit. Ut libero lectus, porta nec turpis sit 
-                                amet, lobortis fringilla ante.                                
-                            </p>
-                            <img className="avatar" src="/src/project/interface/ui/avatar.svg" alt="" />
+                            <div className="author">
+                            <img className="avatar" src="/src/project/interface/assets/avatar.svg" alt="" />
+                            <div>
                             <h6>Celvin Gabriel</h6>
                             <p className="role">Customer</p>
+                            </div>
                             <img className="quotes" src="/src/project/interface/ui/quotes.svg" alt="" />
+                            </div>
+                        </div>
+
+                        <div className="item">
+                            <img className="stars" src="/src/project/interface/ui/5_stars.svg" alt="" />
+                            <p className="body-text">
+                                Lorem ipsum dolor sit amet, consectetur adipis 
+                                cing elit. Ut libero lectus, porta nec turpis sit 
+                                amet, lobortis fringilla ante.                                
+                            </p>
+                            <div className="author">
+                            <img className="avatar" src="/src/project/interface/assets/avatar.svg" alt="" />
+                            <div>
+                            <h6>Celvin Gabriel</h6>
+                            <p className="role">Customer</p>
+                            </div>
+                            <img className="quotes" src="/src/project/interface/ui/quotes.svg" alt="" />
+                            </div>
+                        </div>
+
+                        <div className="item">
+                            <img className="stars" src="/src/project/interface/ui/5_stars.svg" alt="" />
+                            <p className="body-text">
+                                Lorem ipsum dolor sit amet, consectetur adipis 
+                                cing elit. Ut libero lectus, porta nec turpis sit 
+                                amet, lobortis fringilla ante.                                
+                            </p>
+                            
+                            <div className="author">
+                            <img className="avatar" src="/src/project/interface/assets/avatar.svg" alt="" />
+                            <div>
+                            <h6>Celvin Gabriel</h6>
+                            <p className="role">Customer</p>
+                            </div>
+                            <img className="quotes" src="/src/project/interface/ui/quotes.svg" alt="" />
+                            </div>
+
+                            
                         </div>
 
                     </div>
