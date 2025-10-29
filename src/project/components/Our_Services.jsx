170f1d0a5781de091ca08d5fc3b5b3fe21881bfb
JoachimPersson88 <joachim.persson88@gmail.com>
2025-10-29 22:24:18 +0100

    "Home_Our_Services Small Fix" 'Our_Services.jsx, our_services.css' Liten fix på en h4 färg, samt snyggade till lite i jsx-filen

--- src/project/components/Our_Services.jsx
+++ src/project/components/Our_Services.jsx
@@ -10,39 +10,39 @@ export default function Hero() {
                         Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo.
                         Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.
                     </p>
-                                    </div>
+                </div>
 
-                    <div className="our_services_grid">
-                        <div className="item1">
-                            <h4>Diverse Unit Sizes</h4>
-                            <p className="body-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                            </p>
-                        </div>
-                        <div className="item2">
-                            <h4>Moving Assistance</h4>
-                            <p className="body-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                            </p>
-                        </div>
-                        <div className="item3">
-                            <h4>Vehicle Storage</h4>
-                            <p className="body-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                            </p>
-                        </div>
-                        <div className="item4">
-                            <h4>Top-Notch Security</h4>
-                            <p className="body-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                            </p>
-                        </div>
-                        <img src="/src/project/interface/assets/bg_img.svg" alt="" />
+                <div className="our_services_grid">
+                    <div className="item1">
+                        <h4>Diverse Unit Sizes</h4>
+                        <p className="body-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                        </p>
                     </div>
+                    <div className="item2">
+                        <h4>Moving Assistance</h4>
+                        <p className="body-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                        </p>
+                    </div>
+                    <div className="item3">
+                        <h4>Vehicle Storage</h4>
+                        <p className="body-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                        </p>
+                    </div>
+                    <div className="item4">
+                        <h4>Top-Notch Security</h4>
+                        <p className="body-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                        </p>
+                    </div>
+                    <img src="/src/project/interface/assets/bg_img.svg" alt="" />
+                </div>
             </div>
         </section>
     );
