aa23f96f333b911410a04c519c9c9719e90dad18
JoachimPersson88 <joachim.persson88@gmail.com>
2025-10-30 18:25:10 +0100

    "Home_Why_Choose_Us" 'Why_Choose_Us.jsx' Har skapat struktur för jsx-filen, samt fyllt i all information fråån figma och jag har även lagt till behövande ikoner för listan

--- src/project/components/Why_Choose_Us.jsx
+++ src/project/components/Why_Choose_Us.jsx
@@ -1,7 +1,69 @@
 export default function Why_Choose_Us() {
   return (
     <section className="WhyChooseUs">
+      <div className="container">
+        <div className="wcu_area">
+          <h4>Why Choose Us</h4>
+          <h3>Choose Us for Exceptional<br />Storage Solutions</h3>
 
+          <ul className="wcu_list">
+            <li className="list_item">
+              <img src="/src/project/interface/ui/Lock.svg" alt="" />
+              <div>
+                <h5>Security and Safety</h5>
+                <p className="body-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                  luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                </p>
+              </div>
+            </li>
+
+            <li className="list_item">
+              <img src="/src/project/interface/ui/Money_Bag.svg" alt="" />
+              <div>
+                <h5>Flexible and Affordable</h5>
+                <p className="body-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                  luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                </p>
+              </div>
+            </li>
+
+            <li className="list_item">
+              <img src="/src/project/interface/ui/Clean.svg" alt="" />
+              <div>
+                <h5>Clean and Well-Maintained Facilities</h5>
+                <p className="body-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                  luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                </p>
+              </div>
+            </li>
+
+            <li className="list_item">
+              <img src="/src/project/interface/ui/Clock.svg" alt="" />
+              <div>
+                <h5>24/7 and Convenient Access</h5>
+                <p className="body-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                  luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                </p>
+              </div>
+            </li>
+          </ul>
+
+          <div className="wcu_grid">
+            <p className="body-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui
+              lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue.
+              Phasellus eget elit gravida.
+            </p>
+
+            <img src="/src/project/interface/assets/bg_img.svg" alt="" />
+          </div>
+        </div>
+        
+      </div>
     </section>
   );
 }
