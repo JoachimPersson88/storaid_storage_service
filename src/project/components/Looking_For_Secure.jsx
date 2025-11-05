d4b381985dd240aceee0de9b8b6f22383dad1772
JoachimPersson88 <joachim.persson88@gmail.com>
2025-11-05 13:33:50 +0100

    "Home_Looking_For_Secure" 'Looking_For_Secure.jsx' Har fixat strukturen och fyllt i informationen.

--- src/project/components/Looking_For_Secure.jsx
+++ src/project/components/Looking_For_Secure.jsx
@@ -1,7 +1,22 @@
 export default function Looking_For_Service() {
     return (
         <section className="looking_for_service">
+            <div className="container">
 
+                <h3 className="header">Looking for Secure and Flexible Storage? Find
+                    <br/>
+                    Your Perfect Fit With Us.
+                    </h3>
+
+                <p className="body-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo.
+                    Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.
+                    </p>
+                    
+                    <button className="btn-book">Book Now</button>
+
+            </div>
         </section>
     );
 }
