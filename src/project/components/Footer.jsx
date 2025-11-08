36bbaa28d97d4ac69dcfd7f6dcb6a803cd862d38
JoachimPersson88 <joachim.persson88@gmail.com>
2025-11-08 15:18:45 +0100

    "Home_Footer" 'Footer.jsx, footer.css' Har fixat strukturen och stylingen

--- src/project/components/Footer.jsx
+++ src/project/components/Footer.jsx
@@ -1,9 +1,26 @@
 export default function Footer() {
+    const year = new Date().getFullYear();
+
     return (
-        <section className="footer">
+        <footer className="footer">
             <div className="container">
 
+                <p className="copyright body-text">
+                    © {year} StorAid, All rights reserved.
+                </p>
+
+                <div className="legal">
+                    <a className="body-text" href="#">
+                        <img src="src/project/interface/ui/Footer_Icon.svg" alt="" />
+                        Terms & Conditions
+                    </a>
+                    <a className="body-text" href="#">
+                        <img src="src/project/interface/ui/Footer_Icon.svg" alt="" />
+                        Privacy Policy
+                    </a>
+                </div>
+
             </div>
-        </section>
+        </footer>
     );
 }
