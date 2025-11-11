6a477804e03abb2ec7278898e1992c10469e99e5
JoachimPersson88 <joachim.persson88@gmail.com>
2025-11-11 18:20:00 +0100

    "Testimonials START" 'Testimonials.js, Testimonials.jsx' Har fått in api fetch som hämtar api;et från azure. Detta fick jag ta hjälp av chatGPT och jag kollade såklart på skolans videos.

--- src/project/components/Testimonials.jsx
+++ src/project/components/Testimonials.jsx
@@ -1,80 +1,70 @@
+import { useEffect, useState } from "react";
+import { fetchTestimonials } from "../api/testimonials";
+
 export default function Testimonials() {
-    return (
-        <section className="testimonials">
-            
-            <img className="Bg" src="/src/project/interface/assets/Testimonials_Bg.svg" alt="" />
-            <div className="container">
-                <div className="testimonials_area">
+  // items = lista med testimonials, setItems = funktion för att uppdatera listan
+  const [items, setItems] = useState([]);
 
-                    <h4>Testimonials</h4>
-                    <h3>See What Our Client Have to Say</h3>
-                    <p className="body-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo.
-                        Mauris ac blandit
-                        <br/>nisi non sodales augue. Phasellus eget elit gravida.
-                        </p>
+  // Körs en gång efter första render (tom beroendelista [])
+  // Hämtar testimonials och lägger dem i state.
+  useEffect(() => {
+    fetchTestimonials().then(setItems).catch(() => setItems([]));
+  }, []);
 
-                    <div className="testimonials_grid">
+  return (
+    <section className="testimonials">
+      {/* Bakgrundsbild för sektionen */}
+      <img className="Bg" src="/src/project/interface/assets/Testimonials_Bg.svg" alt="" />
+      <div className="container">
+        <div className="testimonials_area">
 
-                        <div className="item">
-                            <img className="stars" src="/src/project/interface/ui/4_stars.svg" alt="" />
-                            <p className="body-text">
-                                Lorem ipsum dolor sit amet, consectetur adipis 
-                                cing elit. Ut libero lectus, porta nec turpis sit 
-                                amet, lobortis fringilla ante.
-                                </p>
-                            <div className="author">
-                            <img className="avatar" src="/src/project/interface/assets/avatar.svg" alt="" />
-                            <div>
-                            <h6>Aiden Harvey</h6>
-                            <p className="role">Customer</p>
-                            </div>
-                            <img className="quotes" src="/src/project/interface/ui/quotes.svg" alt="" />
-                            </div>
-                        </div>
+          {/* Sektionens rubriker och introtext */}
+          <h4>Testimonials</h4>
+          <h3>See What Our Client Have to Say</h3>
+          <p className="body-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida.
+            Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.
+          </p>
 
-                        <div className="item">
-                            <img className="stars" src="/src/project/interface/ui/5_stars.svg" alt="" />
-                            <p className="body-text">
-                                Lorem ipsum dolor sit amet, consectetur adipis 
-                                cing elit. Ut libero lectus, porta nec turpis sit 
-                                amet, lobortis fringilla ante.                                
-                            </p>
-                            <div className="author">
-                            <img className="avatar" src="/src/project/interface/assets/avatar.svg" alt="" />
-                            <div>
-                            <h6>Carrisa Jocelyn</h6>
-                            <p className="role">Customer</p>
-                            </div>
-                            <img className="quotes" src="/src/project/interface/ui/quotes.svg" alt="" />
-                            </div>
-                        </div>
+          <div className="testimonials_grid">
+            {items.map(testimonials => {
+              // Väljer stjärn-ikon beroende på rating
+              const stars = Number(testimonials.rating) >= 5
+                ? "/src/project/interface/ui/5_stars.svg" // Denna infaller om det är 5
+                : "/src/project/interface/ui/4_stars.svg"; // Denna infaller om det inte är 5
 
-                        <div className="item">
-                            <img className="stars" src="/src/project/interface/ui/5_stars.svg" alt="" />
-                            <p className="body-text">
-                                Lorem ipsum dolor sit amet, consectetur adipis 
-                                cing elit. Ut libero lectus, porta nec turpis sit 
-                                amet, lobortis fringilla ante.                                
-                            </p>
-                            
-                            <div className="author">
-                            <img className="avatar" src="/src/project/interface/assets/avatar.svg" alt="" />
-                            <div>
-                            <h6>Celvin Gabriel</h6>
-                            <p className="role">Customer</p>
-                            </div>
-                            <img className="quotes" src="/src/project/interface/ui/quotes.svg" alt="" />
-                            </div>
+              return (
+                // Enskilt kort. key behövs för effektiv rendering i listor
+                <div className="item">
 
-                            
-                        </div>
+                  {/* Rating */}
+                  <img className="stars" src={stars} alt={`${testimonials.rating ?? 5} out of 5`} />
 
+                  {/* Själva kommentaren */}
+                  <p className="body-text">{testimonials.comment}</p>
+
+                  {/* Avatar*/}
+                  <div className="author">
+                    <img
+                      className="avatar"
+                      src={testimonials.avatarUrl}
+                    />
+
+                    {/* Namn och roll */}
+                    <div>
+                      <h6>{testimonials.name}</h6>
+                      <p className="role">{testimonials.companyName}</p>
                     </div>
 
+                    <img className="quotes" src="/src/project/interface/ui/quotes.svg" alt="" />
+                  </div>
                 </div>
-            </div>
-        </section>
-    );
+              );
+            })}
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
 }
