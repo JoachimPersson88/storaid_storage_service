6a4d2915d153c7f27798652d92f00cf278851d57
JoachimPersson88 <joachim.persson88@gmail.com>
2025-11-13 21:17:59 +0100

    "FAQ" Skapat en liten struktur i html och css, tänkte göra en accordion som en seperat komponent.

--- src/project/api/testimonials.js
+++ src/project/api/testimonials.js
@@ -3,11 +3,7 @@ const API_BASE = "https://win25-jsf-assignment.azurewebsites.net";
 export async function fetchTestimonials() {
   try {
     const res = await fetch(`${API_BASE}/api/testimonials`);
-    // Utför ett HTTP GET-anrop till `${API_BASE}/.....`.
-    // $-tecknet är en del av template literals (mallsträngar) i JavaScript.
-
     if (!res.ok) throw new Error(`HTTP ${res.status}`);
-    // Detta gör att vi hoppar till catch-blocket istället för att försöka läsa body.
 
     return await res.json(); 
   } catch (err) {
--- src/project/components/FAQ.jsx
+++ src/project/components/FAQ.jsx
@@ -1,8 +1,24 @@
 export default function FAQ() {
+
+
     return (
         <section className="faq">
             <div className="container">
+                <div className="header">
+                    <h4>
+                        FAQs
+                    </h4>
+                    <h3>
+                        Frequently Ask Questions
+                    </h3>
+                    <p className="body-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+                    </p>
+                    <div className="faqs">
 
+                    </div>
+                </div>
             </div>
         </section>
     );
