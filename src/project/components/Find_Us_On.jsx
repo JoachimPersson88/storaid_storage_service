2eda1889502e938647824a6a72cbcd71fe4a05db
JoachimPersson88 <joachim.persson88@gmail.com>
2025-11-14 15:49:58 +0100

    "Find_Us_On START" Har fixat alla behövande filer, samt fixat alla bilder/ikoner och jag har även fixat pathingen.

---
+++ src/project/components/Find_Us_On.jsx
@@ -0,0 +1,11 @@
+export default function Find_Us_On() {
+
+
+    return (
+        <section className="find_us">
+            <div className="container">
+
+            </div>
+        </section>
+    );
+}
--- src/project/pages/Contact_Page.jsx
+++ src/project/pages/Contact_Page.jsx
@@ -2,6 +2,7 @@ import Subscribe_Our_Newsletter from "../components/Subscribe_Our_Newsletter";
 import Footer from "../components/Footer";
 import Get_in_Touch from "../components/Get_in_Touch";
 import FAQ from "../components/FAQ";
+import Find_Us_On from "../components/Find_Us_On";
 
 export default function Contact_Page() {
   return (
@@ -19,7 +20,7 @@ export default function Contact_Page() {
 
       </section>
       <Get_in_Touch />
-      Find Us On
+      <Find_Us_On />
       <FAQ />
       <Subscribe_Our_Newsletter />
       <Footer />
